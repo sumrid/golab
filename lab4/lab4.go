e06b733c485af80a3d4716dfd6a291acaddb145c
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-06-07 15:39:43 +0700

    update

--- lab4/lab4.go
+++ lab4/lab4.go
@@ -3,6 +3,7 @@ package main
 import (
 	"sort"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,6 +20,8 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/lab4", separateParagraph)
+	r.GET("/gettime", getTimeEndpoint)
+	r.POST("/postting", posttingEndpoint)
 
 	r.Run(":8000")
 }
@@ -45,4 +48,51 @@ func separateParagraph(c *gin.Context) {
 
 	// return
 	c.JSON(200, wordRes)
+}
+
+type Request struct {
+	Time string
+}
+
+func getServerTime() time.Time {
+	return time.Now()
+}
+
+func getTimeEndpoint(c *gin.Context) {
+	t := getServerTime()
+	trq := Request{}
+	trq.Time = t.Format("02/01/2006 15:04:05")
+
+	c.JSON(200, trq)
+	// c.JSON(200, t.Format("Mon Jan 02-01-2006 15:04:05"))
+	// c.JSON(200, t.Format(time.StampMicro))
+}
+
+func posttingEndpoint(c *gin.Context) {
+	type Input struct {
+		A int `json:"a"`
+		B int `json:"b"`
+	}
+	type Response struct {
+		A       int
+		B       int
+		Sum     int
+		HourSum int
+		Time    string
+	}
+
+	t := getServerTime()
+
+	// bind JSON
+	input := Input{}
+	c.BindJSON(&input)
+
+	// return JSON
+	res := Response{}
+	res.A = input.A
+	res.B = input.B
+	res.Sum = input.A + input.B
+	res.HourSum = t.Day() + res.Sum
+	res.Time = t.Format(time.ANSIC)
+	c.JSON(200, res)
 }
